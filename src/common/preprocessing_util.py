b7a5754b69de4e212f59131df1a98a9fbdace03a
Dilshan Wijayarathne <dilshannethmin77@gmail.com>
2026-06-20 00:03:10 +0530

    add

---
+++ src/common/preprocessing_util.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def replace_zeros_with_mean(X):
+    X=X.copy() 
+    cols = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
+    for col in cols:
+        if col in X.columns:
+            X[col]= X[col].replace(0, np.nan)
+    return X
+
+   
